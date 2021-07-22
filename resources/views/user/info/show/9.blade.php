4a9c87252f9dd5a82a21768a4a573d2295eca48a
obata <79499875+Obata-Hiromi@users.noreply.github.com>
2021-07-22 15:00:21 +0900

    Update 9.blade.php

--- resources/views/user/info/show/9.blade.php
+++ resources/views/user/info/show/9.blade.php
@@ -1,11 +1,13 @@
 <h2>医療</h2>
-	<table class="table">
-		<tr><td>平熱</td><td>：{{$base->info()->info["care1"]}}</td></tr>		
+<div class="table-responsive">
+	<table class="table text-nowrap"> 
+		<tr><td style="width: 30%">平熱</td><td style="width: 70%">：{{$base->info()->info["care1"]}}</td></tr>		
 		<tr><td>身長</td><td>：{{$base->info()->info["care2"]}}</td></tr>		
 		<tr><td>体重</td><td>：{{$base->info()->info["care3"]}}</td></tr>		
-	    <tr><th>薬の使用状況</th><td></td></tr>
+		<tr><th>薬の使用状況</th><td></td></tr>
 		<tr><td>アレルギー歴</td><td>：{{$base->info()->info["care4"]}}</td></tr>	
 		<tr><td>既往歴</td><td>：{{$base->info()->info["care5"]}}</td></tr>				
 		<tr><td>手術歴</td><td>：{{$base->info()->info["care6"]}}</td></tr>				
 		<tr><td>かかりつけの病院</td><td>：{{$base->info()->info["care7"]}}</td></tr>	
 	</table>
+</div>
