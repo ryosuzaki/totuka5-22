877c2979d1c573483a73028a13cccc479e29543c
obata <79499875+Obata-Hiromi@users.noreply.github.com>
2021-07-25 12:02:14 +0900

    Update 9.blade.php

--- resources/views/user/info/show/9.blade.php
+++ resources/views/user/info/show/9.blade.php
@@ -1,13 +1,13 @@
 <h2>医療</h2>
 <div class="table-responsive">
 	<table class="table text-nowrap"> 
-		<tr><td style="width: 30%">平熱</td><td style="width: 70%">{{$base->info()->info["care1"]}}℃</td></tr>		
-		<tr><td>身長</td><td>{{$base->info()->info["care2"]}}cm</td></tr>		
-		<tr><td>体重</td><td>{{$base->info()->info["care3"]}}kg</td></tr>		
-		<tr><th>薬の使用状況</th><td></td></tr>
-		<tr><td>アレルギー歴</td><td>{{$base->info()->info["care4"]}}</td></tr>	
-		<tr><td>既往歴</td><td>{{$base->info()->info["care5"]}}</td></tr>				
-		<tr><td>手術歴</td><td>{{$base->info()->info["care6"]}}</td></tr>				
-		<tr><td>かかりつけの病院</td><td>{{$base->info()->info["care7"]}}</td></tr>	
+		<tr><td style="width: 30%">平熱</td><td style="width: 70%">{{$base->info()->info["temp"]}}℃</td></tr>		
+		<tr><td>身長</td><td>{{$base->info()->info["height"]}}cm</td></tr>		
+		<tr><td>体重</td><td>{{$base->info()->info["weight"]}}kg</td></tr>		
+		<tr><th>薬の使用状況</th><td>{{$base->info()->info["medicine"]}}</td></tr>
+		<tr><td>アレルギー歴</td><td>{{$base->info()->info["allergy"]}}</td></tr>	
+		<tr><td>既往歴</td><td>{{$base->info()->info["medical"]}}</td></tr>				
+		<tr><td>手術歴</td><td>{{$base->info()->info["surgery"]}}</td></tr>				
+		<tr><td>かかりつけの病院</td><td>{{$base->info()->info["hospital"]}}</td></tr>	
 	</table>
 </div>
